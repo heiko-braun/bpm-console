5be1587418f181bddf39e469bc237ed56d46d397
heiko-braun <hbraun@redhat.com>
2010-09-23 17:53:32 +0200

    Integrate activity monitor prototype

--- gui/war/src/main/java/org/jboss/bpm/console/client/ErraiApplication.java
+++ gui/war/src/main/java/org/jboss/bpm/console/client/ErraiApplication.java
@@ -37,8 +37,10 @@ import org.jboss.bpm.console.client.icons.ConsoleIconBundle;
 import org.jboss.bpm.console.client.util.ConsoleLog;
 import org.jboss.errai.bus.client.api.Message;
 import org.jboss.errai.bus.client.api.MessageCallback;
+import org.jboss.errai.bus.client.api.SubscribeListener;
 import org.jboss.errai.bus.client.framework.ClientMessageBus;
 import org.jboss.errai.bus.client.ErraiBus;
+import org.jboss.errai.bus.client.framework.SubscriptionEvent;
 import org.jboss.errai.bus.client.security.SecurityService;
 import org.jboss.errai.workspaces.client.framework.Registry;
 import org.jboss.errai.workspaces.client.api.annotations.DefaultBundle;
@@ -49,121 +51,127 @@ import org.jboss.errai.workspaces.client.api.annotations.GroupOrder;
  */
 @GroupOrder("Tasks, Processes, Reporting, Runtime, Settings")
 @DefaultBundle(ConsoleIconBundle.class)
-public class ErraiApplication implements EntryPoint
+public class ErraiApplication implements EntryPoint, SubscribeListener
 {
-  public void onModuleLoad() {
-    Log.setUncaughtExceptionHandler();
+    public void onModuleLoad() {
+        Log.setUncaughtExceptionHandler();
 
-    DeferredCommand.addCommand(new Command() {
-      public void execute() {
+        DeferredCommand.addCommand(new Command() {
+            public void execute() {
 
-        // hide splash image
-        // move the loading div to background
-        DOM.getElementById("splash_loading").getStyle().setProperty("display", "none");
-        DOM.getElementById("splash").getStyle().setProperty("zIndex", "-1");
+                // hide splash image
+                // move the loading div to background
+                DOM.getElementById("splash_loading").getStyle().setProperty("display", "none");
+                DOM.getElementById("splash").getStyle().setProperty("zIndex", "-1");
 
-        onModuleLoad2();
-      }
-    });
-
-    ErraiBus.get().subscribe(Model.SUBJECT,
-        new MessageCallback()
-        {
-          public void callback(Message message)
-          {
-            Log.debug("Data model: " + message.getCommandType());
-          }
+                onModuleLoad2();
+            }
         });
-  }
 
-  public void onModuleLoad2()
-  {
-    final ClientMessageBus bus = (ClientMessageBus) ErraiBus.get();
-
-    bus.addPostInitTask(
-        new Runnable() {
-          public void run()
-          {
-            Registry.get(SecurityService.class).setDeferredNotification(true);
-          }
-        }
-    );
-
-    Controller mainController = new com.mvc4g.client.Controller();
-    Registry.set(Controller.class, mainController);
-
-    // ------
-
-    // setup base urls
-    String proxyUrl = null;
-    if (!GWT.isScript())
-    {
-      proxyUrl = GWT.getModuleBaseURL() + "xhp";
+        ErraiBus.get().subscribe(Model.SUBJECT,
+                new MessageCallback()
+                {
+                    public void callback(Message message)
+                    {
+                        Log.debug("Data model: " + message.getCommandType());
+                    }
+                });
     }
 
-    final ConsoleConfig config = new ConsoleConfig(proxyUrl);
-    ConsoleLog.debug("Console server: " + config.getConsoleServerUrl());
-
-    URLBuilder.configureInstance(config);
-
-    // ------
-
-    ApplicationContext appContext = new ApplicationContext()
+    public void onModuleLoad2()
     {
-      
-      public void displayMessage(String message, boolean isError)
-      {
-        if(isError)
-          MessageBox.error("Error", message);
-        else
-          MessageBox.alert("Warn", message);
-      }
+        final ClientMessageBus bus = (ClientMessageBus) ErraiBus.get();
 
-      
-      public Authentication getAuthentication()
-      {
-        return Registry.get(Authentication.class); // set in login view
-      }
+        bus.addPostInitTask(
+                new Runnable() {
+                    public void run()
+                    {
+                        Registry.get(SecurityService.class).setDeferredNotification(true);
+                    }
+                }
+        );
 
-      
-      public ConsoleConfig getConfig()
-      {
-        return config;
-      }
+        bus.addSubscribeListener(this);
+        
+        Controller mainController = new com.mvc4g.client.Controller();
+        Registry.set(Controller.class, mainController);
 
-      
-      public void refreshView()
-      {
+        // ------
 
-      }
-    };
+        // setup base urls
+        String proxyUrl = null;
+        if (!GWT.isScript())
+        {
+            proxyUrl = GWT.getModuleBaseURL() + "xhp";
+        }
 
-    Registry.set(ApplicationContext.class, appContext);
+        final ConsoleConfig config = new ConsoleConfig(proxyUrl);
+        ConsoleLog.debug("Console server: " + config.getConsoleServerUrl());
 
-    // ------
+        URLBuilder.configureInstance(config);
 
-    registerGlobalViewsAndActions(mainController);
+        // ------
 
-    mainController.addAction("login", new LoginAction());
-    mainController.addAction(BootstrapAction.ID, new BootstrapAction());
-    mainController.addView("loginView", new LoginView());
+        ApplicationContext appContext = new ApplicationContext()
+        {
 
-    // bootstrap and login
-    mainController.handleEvent(
-        new com.mvc4g.client.Event(BootstrapAction.ID, Boolean.TRUE)
-    );
+            public void displayMessage(String message, boolean isError)
+            {
+                if(isError)
+                    MessageBox.error("Error", message);
+                else
+                    MessageBox.alert("Warn", message);
+            }
 
-    mainController.handleEvent(new Event("login", null));
-  }
 
-  /**
-   * Views and actions accessible from any component
-   */
-  private void registerGlobalViewsAndActions(Controller controller)
-  {
-    // register global views and actions, available across editors
-    //controller.addView(Header.ID, header);
-    controller.addAction(LoadingStatusAction.ID, new LoadingStatusAction());
-    controller.addAction(BootstrapAction.ID, new BootstrapAction());       
-  }
+            public Authentication getAuthentication()
+            {
+                return Registry.get(Authentication.class); // set in login view
+            }
+
+
+            public ConsoleConfig getConfig()
+            {
+                return config;
+            }
+
+
+            public void refreshView()
+            {
+
+            }
+        };
+
+        Registry.set(ApplicationContext.class, appContext);
+
+        // ------
+
+        registerGlobalViewsAndActions(mainController);
+
+        mainController.addAction("login", new LoginAction());
+        mainController.addAction(BootstrapAction.ID, new BootstrapAction());
+        mainController.addView("loginView", new LoginView());
+
+        // bootstrap and login
+        mainController.handleEvent(
+                new com.mvc4g.client.Event(BootstrapAction.ID, Boolean.TRUE)
+        );
+
+        mainController.handleEvent(new Event("login", null));
+    }
+
+    /**
+     * Views and actions accessible from any component
+     */
+    private void registerGlobalViewsAndActions(Controller controller)
+    {
+        // register global views and actions, available across editors
+        //controller.addView(Header.ID, header);
+        controller.addAction(LoadingStatusAction.ID, new LoadingStatusAction());
+        controller.addAction(BootstrapAction.ID, new BootstrapAction());
+    }
+
+    public void onSubscribe(SubscriptionEvent event) {
+        ConsoleLog.debug("New Subscription: "+event.getSubject());
+    }
 }
